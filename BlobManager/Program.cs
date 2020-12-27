2ed6253f16d75b994b14931cc420e157226a378c
trivik78 <unixvivek@live.com>
2020-12-27 12:08:12 +0530

    init

---
+++ BlobManager/Program.cs
@@ -0,0 +1,78 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    private const string blobServiceEndpoint = "https://iamdemosa01.blob.core.windows.net/";
+    private const string storageAccountName = "iamdemosa01";
+    private const string storageAccountKey = "https://iamdemosa01.blob.core.windows.net/?sv=2019-12-12&ss=bfqt&srt=c&sp=rwdlacup&se=2020-12-27T14:22:03Z&st=2020-12-27T06:22:03Z&spr=https&sig=%2F4HaPNQK9O3vQsiHBqlKVaarG1E5ipLYb5H2U30V480%3D";
+
+    public static async Task Main(string[] args)
+    {
+        StorageSharedKeyCredential accountCredentials = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+
+        BlobServiceClient serviceClient = new BlobServiceClient(new Uri(blobServiceEndpoint), accountCredentials);
+
+        AccountInfo info = await serviceClient.GetAccountInfoAsync();
+
+        await Console.Out.WriteLineAsync($"Connected to Azure Storage Account");
+        await Console.Out.WriteLineAsync($"Account name:\t{storageAccountName}");
+        await Console.Out.WriteLineAsync($"Account kind:\t{info?.AccountKind}");
+        await Console.Out.WriteLineAsync($"Account sku:\t{info?.SkuName}");
+        Console.ReadKey();
+
+        await EnumerateContainersAsync(serviceClient);
+
+        string existingContainerName = "raster-graphics";
+        await EnumerateBlobsAsync(serviceClient, existingContainerName);
+    
+        string newContainerName = "vector-graphics";
+        BlobContainerClient containerClient = await GetContainerAsync(serviceClient, newContainerName);
+    
+        string uploadedBlobName = "graph.svg";
+        BlobClient blobClient = await GetBlobAsync(containerClient, uploadedBlobName);
+
+        await Console.Out.WriteLineAsync($"Blob Url:\t{blobClient.Uri}");
+    }
+
+    private static async Task EnumerateContainersAsync(BlobServiceClient client)
+    {
+        await foreach (BlobContainerItem container in client.GetBlobContainersAsync())
+        {
+            await Console.Out.WriteLineAsync($"Container:\t{container.Name}");
+        }
+    }
+
+    private static async Task EnumerateBlobsAsync(BlobServiceClient client, string containerName)
+    {      
+        BlobContainerClient container = client.GetBlobContainerClient(containerName);
+        
+        await Console.Out.WriteLineAsync($"Searching:\t{container.Name}");
+        
+        await foreach (BlobItem blob in container.GetBlobsAsync())
+        {        
+             await Console.Out.WriteLineAsync($"Existing Blob:\t{blob.Name}");
+        }
+    }
+
+    private static async Task<BlobContainerClient> GetContainerAsync(BlobServiceClient client, string containerName)
+    {      
+        BlobContainerClient container = client.GetBlobContainerClient(containerName);
+        
+        await container.CreateIfNotExistsAsync();
+        
+        await Console.Out.WriteLineAsync($"New Container:\t{container.Name}");
+        
+        return container;
+    }
+
+    private static async Task<BlobClient> GetBlobAsync(BlobContainerClient client, string blobName)
+    {      
+        BlobClient blob = client.GetBlobClient(blobName);
+        await Console.Out.WriteLineAsync($"Blob Found:\t{blob.Name}");
+        return blob;
+    }
+}
---
+++ GraphClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GraphClient
+{
+    public class Program
+    {
+        private const string _clientID="6bac34a2-ab76-48be-831f-f5893571272d";
+        private const string _tenantId="2acaa146-956f-4dde-871f-ed300b4e8030";
+        public static async Task  Main(string[] args)
+        {
+            IPublicClientApplication app;
+            app=PublicClientApplicationBuilder
+            .Create(_clientID)
+            .WithAuthority(AzureCloudInstance.AzurePublic,_tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+            Console.WriteLine("Registered Client App!!");
+            List<string> scopes = new List<string>
+            {
+                "user.read"
+            };
+
+            AuthenticationResult result;
+            result = await app
+            .AcquireTokenInteractive(scopes)
+            .ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+
+
+
+
+        }
+    }
+}
